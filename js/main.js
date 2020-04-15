19ef070b5dfdaa449e7264373421741ca1a07878
unknown <abiodun_flb@yahoo.com>
2020-04-15 16:15:10 +0100

    styled winner msg

--- js/main.js
+++ js/main.js
@@ -1,95 +1,100 @@
-var move = 1;
-var play = true;
+$(document).ready(function () {
+    var move = 1;
+    var play = true;
 
-$('table tr td').click(function () {
-    if ($(this).text() == "" && play) {
-        if ((move % 2) == 1) {
-            $(this).append('X');
-            $(this).css('color', 'red');
-        } else {
-            $(this).append('O');
-            $(this).css('color', 'blue');
-        }
-
-        move++;
-
-        if (findWinner() != -1 && findWinner() != "") {
-            if (findWinner() == 'X') {
-                $('#winnerX').css({
-                    'visibility': 'visible',
-                    'display': 'block',
-                    'transition': 'all 1s',
-                    'animation-name': 'intro-winner',
-                    'animation-duration': '1.4s',
-                    'animation-delay': '0.1s',
-                    'animation-fill-mode': 'both'
-                });
-                // $('body').append('<div class="winner"><span>Winner</span>X</div>');
-                // $('body').append('<button onclick="location.reload()">Play Again</button>');
-                // $('.winner').css('background-color', '#61892f');
-                // $('button').css('color', '#61892f');
-            } else if (findWinner() == 'O') {
-                $('#winnerO').css({
-                    'visibility': 'visible',
-                    'display': 'block',
-                    'transition': 'all 1s',
-                    'animation-name': 'intro-winner',
-                    'animation-duration': '1.4s',
-                    'animation-delay': '0.1s',
-                    'animation-fill-mode': 'both'
-                });
-                // $('body').append('<div class="winner"><span>Winner</span>O</div>');
-                // $('body').append('<button onclick="location.reload()">Play Again</button>');
-                // $('.winner').css('background-color', 'blue');
-                // $('button').css({
-                //     'color': 'blue',
-                //     'margin': '1.25rem auto'
-                // });
+    $('table tr td').click(function () {
+        if ($(this).text() == "" && play) {
+            if ((move % 2) == 1) {
+                $(this).append('X');
+                $(this).css('color', 'red');
+            } else {
+                $(this).append('O');
+                $(this).css('color', 'blue');
             }
 
-            play = false;
+            move++;
+
+            if (findWinner() != -1 && findWinner() != "") {
+                if (findWinner() == 'X') {
+                    $('#winnerX').css({
+                        'visibility': 'visible',
+
+                        'transition': 'all 1s',
+                        'animation-name': 'intro-winner',
+                        'animation-duration': '1.4s',
+                        'animation-delay': '0.1s',
+                        'animation-fill-mode': 'both'
+                    });
+                    // $('body').append('<div class="winner"><span>Winner</span>X</div>');
+                    // $('body').append('<button onclick="location.reload()">Play Again</button>');
+                    // $('.winner').css('background-color', '#61892f');
+                    // $('button').css('color', '#61892f');
+                } else {
+                    $('#winnerO').css({
+                        'visibility': 'visible',
+
+                        'transition': 'all 1s',
+                        'animation-name': 'intro-winner',
+                        'animation-duration': '1.4s',
+                        'animation-delay': '0.1s',
+                        'animation-fill-mode': 'both'
+                    });
+                    // $('body').append('<div class="winner"><span>Winner</span>O</div>');
+                    // $('body').append('<button onclick="location.reload()">Play Again</button>');
+                    // $('.winner').css('background-color', 'blue');
+                    // $('button').css({
+                    //     'color': 'blue',
+                    //     'margin': '1.25rem auto'
+                    // });
+                }
+
+                play = false;
+            }
         }
+
+    });
+
+    function findWinner() {
+        var sp1 = $('table tr:nth-child(1) td:nth-child(1').text();
+        var sp2 = $('table tr:nth-child(1) td:nth-child(2').text();
+        var sp3 = $('table tr:nth-child(1) td:nth-child(3)').text();
+        var sp4 = $('table tr:nth-child(2) td:nth-child(1)').text();
+        var sp5 = $('table tr:nth-child(2) td:nth-child(2)').text();
+        var sp6 = $('table tr:nth-child(2) td:nth-child(3)').text();
+        var sp7 = $('table tr:nth-child(3) td:nth-child(1)').text();
+        var sp8 = $('table tr:nth-child(3) td:nth-child(2)').text();
+        var sp9 = $('table tr:nth-child(3) td:nth-child(3)').text();
+
+
+        //find rows
+
+        if ((sp1 == sp2) && (sp2 == sp3)) {
+            return sp3;
+        } else if ((sp4 == sp5) && (sp5 == sp6)) {
+            return sp6;
+
+        } else if ((sp7 == sp8) && (sp8 == sp9)) {
+            return sp9;
+        }
+        //find column
+        else if ((sp1 == sp4) && (sp4 == sp7)) {
+            return sp7;
+        } else if ((sp2 == sp5) && (sp5 == sp8)) {
+            return sp8;
+        } else if ((sp3 == sp6) && (sp6 == sp9)) {
+            return sp9;
+        }
+
+        //find diagonal
+        else if ((sp1 == sp5) && (sp5 == sp9)) {
+            return sp9;
+        } else if ((sp3 == sp5) && (sp5 == sp7)) {
+            return sp7;
+        }
+
+        //no winner 
+        return -1;
+
     }
 
 });
-
-function findWinner() {
-    var sp1 = $('table tr:nth-child(1) td:nth-child(1').text();
-    var sp2 = $('table tr:nth-child(1) td:nth-child(2').text();
-    var sp3 = $('table tr:nth-child(1) td:nth-child(3)').text();
-    var sp4 = $('table tr:nth-child(2) td:nth-child(1)').text();
-    var sp5 = $('table tr:nth-child(2) td:nth-child(2)').text();
-    var sp6 = $('table tr:nth-child(2) td:nth-child(3)').text();
-    var sp7 = $('table tr:nth-child(3) td:nth-child(1)').text();
-    var sp8 = $('table tr:nth-child(3) td:nth-child(2)').text();
-    var sp9 = $('table tr:nth-child(3) td:nth-child(3)').text();
-
-
-    //find rows
-
-    if ((sp1 == sp2) && (sp2 == sp3)) {
-        return sp3;
-    } else if ((sp4 == sp5) && (sp5 == sp6)) {
-        return sp6;
-    } else if ((sp7 == sp8) && (sp8 == sp9)) {
-        return sp9;
-    }
-    //find column
-    else if ((sp1 == sp4) && (sp4 == sp7)) {
-        return sp7;
-    } else if ((sp2 == sp5) && (sp5 == sp8)) {
-        return sp8;
-    } else if ((sp3 == sp6) && (sp6 == sp9)) {
-        return sp9;
-    }
-
-    //find diagonal
-    else if ((sp1 == sp5) && (sp5 == sp9)) {
-        return sp9;
-    } else if ((sp3 == sp5) && (sp5 == sp7)) {
-        return sp7;
-    }
-
-    //no winner 
-    return -1;
-}
